aed1014ccd2fa29fe478c4ba2895d46dd4c5b282
louwenus <louwen.fricout@gmail.com>
2023-10-17 18:00:00 +0200

    pictures

--- src/tldr.c
+++ src/tldr.c
@@ -1,15 +1,25 @@
 #include "tldr.h"
 
-
 struct winsize winsize;
 
 int main (int argc,char **argv,char **env){
 	ioctl(0, TIOCGWINSZ, &winsize);
+	printf("%u %u",winsize.ws_col,winsize.ws_row);
 	setlocale(LC_CTYPE, "");
-    printf("%lc",CHARS_BLOCK_UP);
-	*(int*)0;
+	srand((unsigned int)time(NULL));
+
+	return rand();
 }
 
 void printscreen(int x,int y,int *monstre,char* background){
 	
+}
+
+picture pict_crop_bound(picture pict, uint xmin, uint xmax, uint ymin, uint ymax){
+	return (picture){
+		.col=xmax-xmin+1,
+		.row=ymax-ymin+1,
+		.stride=pict.stride,
+		.data=pict.data+xmin+pict.stride*ymin
+	};
 }
--- src/tldr.h
+++ src/tldr.h
@@ -3,20 +3,24 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
+#include <stdlib.h>
+
 #include "config.h"
+
 enum CHARS {
-	CHARS_BLOCK_UP = U'▀',
-	CHARS_BLOCK_DOWN = U'▄',
-	CHARS_BLOCK_FULL = U'█',
-	CHARS_BLOCK_LEFT = U'▌',
-	CHARS_BLOCK_RIGHT = U'▐',
-	CHARS_BLOCK_LIGHT = U'░',
-	CHARS_BLOCK_MEDIUM = U'▒',
-	CHARS_BLOCK_DARK = U'▓',
-	CHARS_TRIANGLE_LEFT = U'◀',
-	CHARS_TRIANGLE_RIGHT = U'▶',
-	CHARS_TRIANGLE_DOWN = U'▼',
-	CHARS_TRIANGL_UP = U'▼',
+	CHARS_BLOCK_UP = L'▀',
+	CHARS_BLOCK_DOWN = L'▄',
+	CHARS_BLOCK_FULL = L'█',
+	CHARS_BLOCK_LEFT = L'▌',
+	CHARS_BLOCK_RIGHT = L'▐',
+	CHARS_BLOCK_LIGHT = L'░',
+	CHARS_BLOCK_MEDIUM = L'▒',
+	CHARS_BLOCK_DARK = L'▓',
+	CHARS_TRIANGLE_LEFT = L'◀',
+	CHARS_TRIANGLE_RIGHT = L'▶',
+	CHARS_TRIANGLE_DOWN = L'▼',
+	CHARS_TRIANGL_UP = L'▼',
 };
 enum DIRECTIONS {
 	DIR_UP = 0,
@@ -33,3 +37,10 @@ struct monster {
 	int id;
 };
 typedef struct monster monster;
+
+typedef struct {
+	int col;
+	int row;
+	int stride;
+	char32_t* data;
+} picture;
