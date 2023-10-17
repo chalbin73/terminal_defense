668dfa3f7674d085d1232ea25bf94c514e059313
Albin Chaboissier <chalbin73@gmail.com>
2023-10-17 17:11:00 +0200

    Better types

--- src/tldr.h
+++ src/tldr.h
@@ -4,32 +4,40 @@
 #include <stdio.h>
 #include <locale.h>
 #include "config.h"
-enum CHARS {
-	CHARS_BLOCK_UP = L'▀',
-	CHARS_BLOCK_DOWN = L'▄',
-	CHARS_BLOCK_FULL = L'█',
-	CHARS_BLOCK_LEFT = L'▌',
-	CHARS_BLOCK_RIGHT = L'▐',
-	CHARS_BLOCK_LIGHT = L'░',
-	CHARS_BLOCK_MEDIUM = L'▒',
-	CHARS_BLOCK_DARK = L'▓',
-	CHARS_TRIANGLE_LEFT = L'◀',
-	CHARS_TRIANGLE_RIGHT = L'▶',
-	CHARS_TRIANGLE_DOWN = L'▼',
-	CHARS_TRIANGL_UP = L'▼',
-};
-enum DIRECTIONS {
-	DIR_UP = 0,
-	DIR_DOWN = 1,
-	DIR_RIGHT = 2,
-	DIR_LEFT = 3,
+
+enum CHAR
+{
+    CHARS_BLOCK_UP       = L'▀',
+    CHARS_BLOCK_DOWN     = L'▄',
+    CHARS_BLOCK_FULL     = L'█',
+    CHARS_BLOCK_LEFT     = L'▌',
+    CHARS_BLOCK_RIGHT    = L'▐',
+    CHARS_BLOCK_LIGHT    = L'░',
+    CHARS_BLOCK_MEDIUM   = L'▒',
+    CHARS_BLOCK_DARK     = L'▓',
+    CHARS_TRIANGLE_LEFT  = L'◀',
+    CHARS_TRIANGLE_RIGHT = L'▶',
+    CHARS_TRIANGLE_DOWN  = L'▼',
+    CHARS_TRIANGL_UP     = L'▼',
 };
 
-struct monster {
-	char32_t sprite;
-	int direction;
-	int posx;
-	int posy;
-	int id;
+typedef char32_t sprite_t;
+
+enum DIRECTION
+{
+    DIR_UP    = 0,
+    DIR_DOWN  = 1,
+    DIR_RIGHT = 2,
+    DIR_LEFT  = 3,
 };
-typedef struct monster monster ;
+
+struct monster
+{
+    sprite_t     sprite;
+    DIRECTION    direction;
+    int32_t      posx;
+    int32_t      posy;
+    uint32_t     id;
+};
+typedef struct monster monster_t;
+
